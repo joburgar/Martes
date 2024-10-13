1d5347fbef37d7f58154299a23fc2bcf8693c82f
Joanna Burgar <joburgar@Joannas-MBP.lan>
2024-10-13 16:10:24 -0700

    Finding mass (combined).

--- Martes_rangemap.R
+++ Martes_rangemap.R
@@ -15,13 +15,21 @@ citation("mdd")
 # Fernandez ALR (2024). _mdd: Download mammal shapefiles of the world_. R package version 0.1.0,
 # commit 81792d43c28941853b4f6892487ef2862f8c611c, <https://github.com/alrobles/mdd>.
 
+library(dataone)
+citation("dataone")
+# Jones M, Slaughter P, Nahf R, Boettiger C, Jones C, Mecum B, Clark J, Read J, Walker L, Hart E,
+# Chamberlain S (2022). _dataone: R Interface to the DataONE REST API_. doi:10.5063/F1M61H5X
+# <https://doi.org/10.5063/F1M61H5X>, R package version 2.2.2,
+# <https://github.com/DataONEorg/rdataone>.
+
+# Function to download Guloninae ranges and turn to sf objects
 get_sf_range = function(gulsp="Eira barbara") {
   gulmap <- get_mdd_map(species = gulsp)
   sfgulmap <-sf::st_as_sf(gulmap)
   return(sfgulmap)
 }
 
-
+# Create Guloninae range spatial object
 GulSp <- c("Eira barbara","Gulo gulo","Pekania pennanti",
            "Martes flavigula", "Martes gwatkinsii",
            "Martes americana", "Martes caurina", "Martes foina", "Martes martes","Martes melampus", "Martes zibellina")
@@ -79,7 +87,29 @@ ggplot() +
   theme(legend.position = "bottom")+
   theme(legend.title=element_blank())
 
-#### 
+#############################################
+#############################################
+
 # Species Table
-library(MASS)
-MASS:mammals
+
+# NCEAS 2182: Smith: Body paleoecology: Linking temporal and taxonomic scales size in ecology and pattern and process across spatial, National Center Ecological Analysis for and Synthesis, & Felisa Smith. (2004). Macroecological database of mammalian body mass. Knowledge Network for Biocomplexity. 
+# doi:10.5063/AA/nceas.196.3
+
+mamdf <- read.delim(file="mammals.txt", header=F)
+colnames(mamdf) <- c("Continent","Status","Order","Family","Genus","Species","LogMass", "CombinedMass","Ref")
+# Combined mass in grams (adult body mass averaged across males and females)
+mamdf <- as.tibble(mamdf)
+Guldf <- mamdf %>% filter(Genus %in% c("Eira", "Martes", "Gulo"))
+Guldf %>% group_by (Genus, Species) %>% summarise(mean = mean(CombinedMass), min = min(CombinedMass))
+#   Genus  Species      mean    min
+# 1 Eira   barbara     3910   3910 
+# 2 Gulo   gulo       17013. 14525.
+# 3 Martes americana   1250   1250 
+# 4 Martes flavigula   1842.  1185 
+# 5 Martes foina       1541.  1541.
+# 6 Martes gwatkinsii  2043   2043 
+# 7 Martes martes      1300   1300 
+# 8 Martes melampus    1000   1000 
+# 9 Martes nobilis     1950   1950 
+# 10 Martes pennanti    4000   4000 # Pekania pennanti
+# 11 Martes zibellina   1130   1130 
